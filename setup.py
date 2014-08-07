10b8988deca692e443fc78d910462533ecfc7f2b
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-08-07 10:34:00 +0200

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name = "django-crocodoc",
     packages=['dj_crocodoc'],
     description = ("Django app for integrating with CrocoDoc"),
-    version = "0.1.4",
+    version = "0.1.5",
     author = "Ross Crawford-d'Heureuse",
     author_email = "ross@lawpal.com",
     license = "MIT",
